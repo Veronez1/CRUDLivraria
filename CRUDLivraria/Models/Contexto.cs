e6b7a5ce9e29f2f83cd89348712525fe0d8e6471
Veronez1 <daviveronez80@gmail.com>
2022-11-09 16:24:20 -0300

    Adicionar arquivos de projeto.

---
+++ CRUDLivraria/Models/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDLivraria.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto( DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Livro> Livro { get; set; }
+
+    }
+}
